002d9d94374bcb861749111b4a904afc7379c4cf
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-03-28 00:45:33 +0400

    add components from data.json file, responsive

--- index.js
+++ index.js
@@ -4,37 +4,52 @@ import data from './data.json' assert { type: "json" }
 
 let summary = document.getElementById("summary");
 
+function createDomElement(tag, className, category, category_color, icon, color, textContent) {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+
+    if (category) {
+        element.textContent = category;
+    }
+    if (category_color) {
+        element.style.color = category_color;
+    }
+    if (icon) {
+        element.setAttribute("src", icon);
+    }
+    if (color) {
+        element.style.background = color;
+    }
+    if (textContent) {
+        element.textContent = textContent;
+    }
+    return element;
+}
+
 for (let i = 0; i < data.length; i++) {
-    const summary_box = document.createElement("div");
+    
     const { category, category_color, score, icon, color } = data[i];
 
     // box creation
-    summary_box.classList.add("summary-box");
-    summary_box.style.background = color;
+    const summary_box = createDomElement("li", "summary-box", null, null, null, color);
     summary.append(summary_box);
     
     // creation of left container of the box
-    const left_container = document.createElement("div");
-    left_container.classList.add("left-container");
+    const left_container = createDomElement("div", "left-container");
     summary_box.append(left_container);
 
-    const left_container_img = document.createElement("img");
-    left_container_img.setAttribute("src", icon);
+    // creation of img
+    const left_container_img = createDomElement("img", null, null, null, icon);
     left_container.append(left_container_img);
 
-    const category_element = document.createElement("p");
-    category_element.classList.add("category");
-    category_element.textContent = category;
-    category_element.style.color = category_color;
-    left_container.append(category_element);
+    // creation of category text
+    const category_text = createDomElement("p", "category", category, category_color);
+    left_container.append(category_text);
 
+    
     // creation of point system
-
-    const score_element = document.createElement("p");
-    score_element.classList.add("score_paragraph");
-    const out_of_score = document.createElement("span");
-    out_of_score.classList.add("out_of_score");
-    out_of_score.textContent = " / 100";
+    const score_element = createDomElement("p", "score_paragraph");
+    const out_of_score = createDomElement("span", "out_of_score", null, null, null, null, " / 100");
     score_element.append(score, out_of_score);
     summary_box.append(score_element);
 }
